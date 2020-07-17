14bccf6592f2e41b161564fa730905309257f01c
Aveek <Aveek@kl-pt-009.icarasia.local>
2020-07-17 17:45:04 +0800

    Converting the co-ordinates

--- app/src/main/java/com/example/graphviewapp/MainActivity.kt
+++ app/src/main/java/com/example/graphviewapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.graphviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.graphviewapp.customview.GraphView
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     val coordinates = arrayListOf<Pair<Float,Float>>()
+    lateinit var button :Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,12 +18,22 @@ class MainActivity : AppCompatActivity() {
 
         val graphView = findViewById<GraphView>(R.id.graphView)
         graphView.setCoordinatePoints(coordinates)
+
+        initRedrawButton()
     }
 
     private fun initCoordinates() {
-        coordinates.add(Pair(0f,250f))
-        coordinates.add(Pair(250f,250f))
-        coordinates.add(Pair(350f,365f))
-        coordinates.add(Pair(150f,500f))
+        coordinates.add(Pair(1f,2f))
+        coordinates.add(Pair(3f,4f))
+        coordinates.add(Pair(4f,3f))
+        coordinates.add(Pair(5f,5f))
+        coordinates.add(Pair(6f,4f))
+        coordinates.add(Pair(7f,2f))
+    }
+    private fun initRedrawButton(){
+        button = findViewById(R.id.redraw)
+        button.setOnClickListener {
+            graphView.invalidate()
+        }
     }
 }
--- app/src/main/java/com/example/graphviewapp/customview/GraphView.kt
+++ app/src/main/java/com/example/graphviewapp/customview/GraphView.kt
@@ -8,6 +8,7 @@ import android.graphics.Path
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import androidx.annotation.Px
 import com.example.graphviewapp.R
 
 // TODO
@@ -23,9 +24,17 @@ class GraphView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
 
     private var path : Path
     private var paint : Paint
-    private var extraPadding = 10
+    private var extraPadding = 10f
 
     private lateinit var coordinates : ArrayList<Pair<Float,Float>>
+    private var maxXValue : Float = 0f
+    private var maxYValue : Float = 0f
+
+    private var eachPixelAllocationX = 0f
+    private var eachPixelAllocationY = 0f
+
+    private var actualWidth = 0
+    private var actualHeight = 0
 
     init{
         val a = context.obtainStyledAttributes(
@@ -43,12 +52,43 @@ class GraphView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
 
     fun setCoordinatePoints(coordinates : ArrayList<Pair<Float,Float>>){
         this.coordinates = coordinates
+        getMaxCoordinateValues()
         invalidate()
+    }
+
+    private fun getMaxCoordinateValues(){
+        for(i in coordinates){
+            if (i.first > maxXValue) maxXValue = convertDpToPx(i.first)
+            if (i.second > maxYValue) maxYValue = convertDpToPx(i.second)
+        }
+    }
+    private fun convertPxToDp(px: Float) : Float{
+        return px/context.resources.displayMetrics.density
+    }
+    private fun convertDpToPx(dp : Float) :Float{
+        return dp*context.resources.displayMetrics.density
+    }
+    private fun translateToCanvasY( y : Float) : Float{
+        Log.d("canvas y : ", (actualHeight.toFloat() - y).toString())
+//        return convertDpToPx(actualHeight.toFloat()) - y
+        return actualHeight.toFloat() - y
+    }
+    private fun translateToCanvasX(canvas: Canvas, x : Float) : Float{
+        return x
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec)
+        eachPixelAllocationX = MeasureSpec.getSize(widthMeasureSpec)/maxXValue
+        eachPixelAllocationY = MeasureSpec.getSize(heightMeasureSpec)/maxYValue
+        extraPadding = eachPixelAllocationX*2
+        setMeasuredDimension(widthMeasureSpec-extraPadding.toInt(),heightMeasureSpec-extraPadding.toInt())
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        actualWidth = w
+        actualHeight = h
+        super.onSizeChanged(w, h, oldw, oldh)
     }
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -56,12 +96,19 @@ class GraphView @JvmOverloads constructor(context: Context, attrs: AttributeSet?
     }
 
     private fun drawCoordinates(canvas: Canvas) {
-        path.moveTo(0f+extraPadding, 0f+extraPadding)
+//        path.moveTo(0f+extraPadding, translateToCanvasY(0f+extraPadding))
+        path.moveTo(0f+extraPadding, canvas.height+extraPadding)
+
         for (i in coordinates) {
-            path.lineTo(i.first+extraPadding, i.second+extraPadding)
-            path.moveTo(i.first+extraPadding, i.second+extraPadding)
-            Log.d("x : y", i.first.toString() + " : "+i.second.toString())
+            path.lineTo((i.first*eachPixelAllocationX)+extraPadding, translateToCanvasY(i.second*eachPixelAllocationY)+extraPadding)
+            path.moveTo((i.first*eachPixelAllocationX)+extraPadding, translateToCanvasY(i.second*eachPixelAllocationY)+extraPadding)
+//            path.lineTo((i.first*eachPixelAllocationX)+extraPadding, (i.second*eachPixelAllocationY)+extraPadding)
+//            path.moveTo((i.first*eachPixelAllocationX)+extraPadding, (i.second*eachPixelAllocationY)+extraPadding)
+
+            Log.d("x : y = ", i.first.toString() + " : "+i.second.toString())
         }
+        path.lineTo(canvas.width.toFloat(),canvas.height.toFloat())
+        path.moveTo(canvas.width.toFloat(),canvas.height.toFloat())
         path.close()
         canvas.drawPath(path, paint)
     }
